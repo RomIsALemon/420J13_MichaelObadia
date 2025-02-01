0de00512777a64a27c126dc6ea03e06258f9ccf5
RomIsALemon <michaelobadia17@gmail.com>
2025-02-01 17:14:21 -0500

    Finished answering all of the questions for the pointer assignment

--- Assignment2Pointers/Source.cpp
+++ Assignment2Pointers/Source.cpp
@@ -1,8 +1,30 @@
 //This file is being used to check my answers on the txt file
 #include <iostream>
 
-int main() {
-	int a = 10;
-	float* p = (float*)&a;
-	std::cout << *p;
+class Enemy {
+    int* health;
+public:
+    Enemy(int hp) {
+        health = new int(hp);
+    }
+    ~Enemy() {
+        delete health;
+    }
+    void attack() {
+        *health -= 10;
+        std::cout << "Enemy health: " << *health << std::endl;
+    }
+};
+
+void spawnEnemy(Enemy*& enemy) { //changed the passed value to a reference, which fixed the code
+    enemy = new Enemy(100);
 }
+
+int main() {
+    Enemy* orc = nullptr;
+    spawnEnemy(orc);
+    orc->attack();
+    delete orc;
+    return 0;
+}
+
