33960770b064e797de99846469dcfe6a77a0bf33
RomIsALemon <michaelobadia17@gmail.com>
2025-02-10 13:26:45 -0500

    Updated Exercise to add more functions and user input

--- Exercise1/Exercise1.cpp
+++ Exercise1/Exercise1.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 struct Course {
@@ -11,12 +12,45 @@ struct Course {
     float project;
 };
 
+int validateNumber(string gradeName) {
+    int n;
+    while (true) {
+        cout << "Enter " << gradeName << " grade: " << endl;
+        cin >> n;
+        if (cin.fail() || n < 0 || n > 100) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Must be a valid number. Try again." << endl;
+            continue;
+        }
+        else {
+            break;
+        }
+    }
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    return n;
+}
+
+Course createCourse(string name, float grade1, float grade2, float grade3) {
+    Course course;
+    course.name = name;
+    course.midterm = grade1;
+    course.exam = grade2;
+    course.project = grade3;
+    return course;
+}
+
+string toString(Course course) {
+    return course.name + ": Midterm Grade : " + std::to_string(course.midterm) + "%, Exam Grade: " + std::to_string(course.exam) + "%, Project Grade: " + std::to_string(course.project);
+}
+
 int main()
 {
 
     const int courseAmount = 3;
     Course courseArr[courseAmount];
-    courseArr[0].name = "English";
+    /*courseArr[0].name = "English";
     courseArr[0].midterm = 40;
     courseArr[0].exam = 50;
     courseArr[0].project = 10;
@@ -29,10 +63,26 @@ int main()
     courseArr[2].name = "History";
     courseArr[2].midterm = 50;
     courseArr[2].exam = 30;
-    courseArr[2].project = 20;
+    courseArr[2].project = 20;*/
 
     for (int i = 0; i < courseAmount; i++) {
-        cout << courseArr[i].name << ": Midterm weight : " << courseArr[i].midterm << "%, Exam weight: " << courseArr[i].exam << "%, Project weight: " << courseArr[i].project << "% \n" << endl;
+        string name;
+        float grade1;
+        float grade2;
+        float grade3;
+        cout << "Enter course name: " << endl;
+        getline(cin, name);
+        while (true) {
+            grade1 = validateNumber("Midterm");
+            grade2 = validateNumber("Exam");
+            grade3 = validateNumber("Project");
+            courseArr[i] = createCourse(name, grade1, grade2, grade3);
+            break;
+        }
+    }
+
+    for (int i = 0; i < courseAmount; i++) {
+        cout << toString(courseArr[i]) << endl;
     }
 }
 
