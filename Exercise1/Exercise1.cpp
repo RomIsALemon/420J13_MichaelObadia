5274f00a653c010d55708fb5e6134cee48b760b0
RomIsALemon <michaelobadia17@gmail.com>
2025-02-17 09:27:05 -0500

    Added dynamic array size

--- Exercise1/Exercise1.cpp
+++ Exercise1/Exercise1.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 struct Course {
@@ -42,14 +43,36 @@ Course createCourse(string name, float grade1, float grade2, float grade3) {
 }
 
 string toString(Course course) {
-    return course.name + ": Midterm Grade : " + std::to_string(course.midterm) + "%, Exam Grade: " + std::to_string(course.exam) + "%, Project Grade: " + std::to_string(course.project);
+    return course.name + ": Midterm Grade : " + std::to_string(course.midterm) + "%, Exam Grade: " + std::to_string(course.exam) + "%, Project Grade: " + std::to_string(course.project) + "%";
+}
+
+int readArraySize() {
+    int size;
+    while (true) {
+        cout << "Enter array size" << endl;
+        cin >> size;
+        if (cin.fail() || size < 0) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Must be a valid number. Try again." << endl;
+            continue;
+        }
+        else {
+            break;
+        }
+    }
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    return size;
 }
 
 int main()
 {
-
-    const int courseAmount = 3;
-    Course courseArr[courseAmount];
+    int courseNum = readArraySize();
+    //const int courseAmount = courseNum;
+    std::vector<Course> courseList;
+    courseList.resize(courseNum);
+    //Course courseArr[courseAmount];
     /*courseArr[0].name = "English";
     courseArr[0].midterm = 40;
     courseArr[0].exam = 50;
@@ -65,7 +88,7 @@ int main()
     courseArr[2].exam = 30;
     courseArr[2].project = 20;*/
 
-    for (int i = 0; i < courseAmount; i++) {
+    for (int i = 0; i < courseList.size(); i++) {
         string name;
         float grade1;
         float grade2;
@@ -76,13 +99,13 @@ int main()
             grade1 = validateNumber("Midterm");
             grade2 = validateNumber("Exam");
             grade3 = validateNumber("Project");
-            courseArr[i] = createCourse(name, grade1, grade2, grade3);
+            courseList[i] = createCourse(name, grade1, grade2, grade3);
             break;
         }
     }
 
-    for (int i = 0; i < courseAmount; i++) {
-        cout << toString(courseArr[i]) << endl;
+    for (int i = 0; i < courseList.size(); i++) {
+        cout << toString(courseList[i]) << endl;
     }
 }
 
